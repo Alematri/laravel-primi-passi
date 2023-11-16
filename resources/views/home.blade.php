617584ab6d8a180207a2347895c5802495e26c76
Alematri <alessandromatricardi@gmail.com>
2023-11-16 15:11:50 +0100

    [First Commit]

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <!-- BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>.
+    <title>Home</title>
+</head>
+<body>
+
+    <header>
+        <div class="d-flex justify-content-center">
+            <a href="/" class="me-2"><button type="button" class="btn btn-primary">HOME</button></a>
+          <a href="/about"><button type="button" class="btn btn-danger">ABOUT</button></a>
+        </div>
+    </header>
+
+    <h1 class="text-center">Hello World! Wellcome in my Home Page!</h1>
+</body>
+</html>
